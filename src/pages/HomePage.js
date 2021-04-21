a2aa10726fb7734896a83f1ba85ca3b5b4e3a41a
thaoxinhtrai77 <nguyenthao6102@gmail.com>
2021-04-21 23:37:18 +0700

    Setup react hook + redux

---
+++ src/pages/HomePage.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewHobby, setActiveHobby } from "../actions/hobby";
+import HobbyList from "../components/Home/HobbyList";
+
+HomePage.propType = {};
+
+const randomNumber = () => {
+	return 1000 + Math.trunc(Math.random() * 9000);
+};
+export default function HomePage() {
+	const hobbyList = useSelector((state) => state.hobby.list);
+	const activeId = useSelector((state) => state.hobby.activeId);
+
+	// const hobbyState = useSelector((state)=>({
+	// 	list: state.hobby.list,
+	// 	active: state.hobby.activeId
+	// }),shallowEqual)
+	const dispatch = useDispatch();
+
+	const handleAddHobbyClick = () => {
+		// random a hobby object: id + title
+		const newId = randomNumber();
+		const newHobby = {
+			id: newId,
+			title: `Hobby ${newId}`,
+		};
+		// dispatch action to add a new hobby to redux store
+		const action = addNewHobby(newHobby);
+		dispatch(action);
+	};
+	const handleHobbyClick = (hobby) => {
+		const action = setActiveHobby(hobby);
+		dispatch(action);
+	};
+	return (
+		<div className="home-page">
+			<h1>REDUX HOOKS - Home Page</h1>
+			<button onClick={handleAddHobbyClick}>Random hobby</button>
+			<HobbyList
+				hobbyList={hobbyList}
+				activeId={activeId}
+				onHobbyClick={handleHobbyClick}
+			/>
+		</div>
+	);
+}
